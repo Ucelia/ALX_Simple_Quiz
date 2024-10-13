6b27073512554880884907feb74b411a588b921a
celia <neevecelia@gmail.com>
2024-10-13 03:29:28 +0200

    Initial commit

---
+++ quiz.js
@@ -0,0 +1,16 @@
+function checkAnswer(){
+    const correctAnswer = "4";
+    var getSelectedValue = document.querySelector('input[name="quiz"]:checked');
+    if(getSelectedValue !=null){
+        userAnswer = getSelectedValue.value;
+    }
+    if(userAnswer == correctAnswer){
+        document.getElementById('feedback').textContent= 'Correct! Well done.';
+    }else{
+        document.getElementById('feedback').textContent='incorrect. Try again!';
+    }
+    
+}
+
+var submitAnswer = document.getElementById('submit-answer');
+submitAnswer.addEventListener('click', checkAnswer);
