7a50453c3d13ee5ae4fa909a044a3dee2b462b3e
celia <neevecelia@gmail.com>
2024-10-13 03:38:42 +0200

    changes

--- quiz.js
+++ quiz.js
@@ -1,7 +1,7 @@
 function checkAnswer(){
     const correctAnswer = "4";
     var userAnswer = document.querySelector('input[name="quiz"]:checked').value;
-    if(userAnswer === correctAnswer){
+    if (userAnswer === correctAnswer){
         document.getElementById('feedback').textContent= 'Correct! Well done.';
     }else{
         document.getElementById('feedback').textContent='incorrect. Try again!';
